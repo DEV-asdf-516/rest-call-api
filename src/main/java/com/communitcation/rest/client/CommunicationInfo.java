5a3f2b3f488a7662d75b7b3c23ec3477dc692ec9
DEV-asdf-516 <dev2asdf@gmail.com>
2024-03-18 21:55:37 +0900

    fix requestData type

--- src/main/java/com/communitcation/rest/client/CommunicationInfo.java
+++ src/main/java/com/communitcation/rest/client/CommunicationInfo.java
@@ -11,14 +11,14 @@ import java.util.Map;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class CommunicationInfo <T>{
+public class CommunicationInfo {
     private String path; // api 경로
     private String scheme; // 스키마(http / https)
     private String host; // 주소
     private String port; // 포트
     private Method method; // http method
     private MediaType mediaType; // 미디어타입
-    private T requestData; // 요청 데이터 문자열
+    private Object requestData; // 요청 데이터 문자열
     private Map<String,String> headers; // 추가적인 헤더
     private RequestFormat requestFormat; // 요청 데이터 형식
     private RestTemplateComponent restClient; // RestTemplate 객체
